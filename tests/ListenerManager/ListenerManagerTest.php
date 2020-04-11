31003a5b6c148e6a7074e487c1b36f87dac5a968
Josua Hunziker <der@digitalwerker.ch>
2020-04-11 13:45:53 +0200

    add more unit tests

--- tests/ListenerManager/ListenerManagerTest.php
+++ tests/ListenerManager/ListenerManagerTest.php
@@ -34,6 +34,9 @@ use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IConfig;
 use OCP\ILogger;
 
+use OCP\IUser;
+use OCP\IGroup;
+
 use OCA\AutoGroups\ListenerManager;
 
 use Test\TestCase;
@@ -57,6 +60,11 @@ class ListenerManagerTest extends TestCase
         $this->eventDispatcher = $this->createMock(IEventDispatcher::class);
         $this->config = $this->createMock(IConfig::class);
         $this->logger = $this->createMock(ILogger::class);
+
+        $this->testUser = $this->createMock(IUser::class);
+        $this->testUser->expects($this->any())
+            ->method('getDisplayName')
+            ->willReturn('Test User');
     }
 
     private function createListenerManager($auto_groups = [], $override_groups = [], $login_hook = false)
@@ -73,10 +81,29 @@ class ListenerManagerTest extends TestCase
         return new ListenerManager($this->groupManager, $this->eventDispatcher, $this->config, $this->logger);
     }
 
-    public function testOnlyCreatedHookOnDefaultConfig()
-    {
-        $lm = $this->createListenerManager();
+    private function initAndReturnCallback($auto_groups = [], $override_groups = []) {
+        $callback = null;
+        $setupCallback = function ($subject) {
+            $callback = $subject;
+            return is_callable($subject);
+        };
 
+        $this->eventDispatcher->expects($this->exactly(3))
+            ->method('addListener')
+            ->withConsecutive(
+                [UserCreatedEvent::class, $this->callback($setupCallback)],
+                [UserAddedEvent::class, $this->callback($setupCallback)],
+                [UserRemovedEvent::class, $this->callback($setupCallback)],
+            );
+
+        $lm = $this->createListenerManager($auto_groups, $override_groups);
+        $lm->setup();
+
+        return $callback;
+    }
+
+    public function testCreatedAddedRemovedHooksWithDefaultSettings()
+    {
         $isCallable = function ($subject) {
             return is_callable($subject);
         };
@@ -89,6 +116,53 @@ class ListenerManagerTest extends TestCase
                 [UserRemovedEvent::class, $this->callback($isCallable)]
             );
 
+        $lm = $this->createListenerManager();
         $lm->setup();
+    }
+
+    public function testAlsoLoginHookIfEnabled() {
+        $isCallable = function ($subject) {
+            return is_callable($subject);
+        };
+
+        $this->eventDispatcher->expects($this->exactly(4))
+            ->method('addListener')
+            ->withConsecutive(
+                [UserCreatedEvent::class, $this->callback($isCallable)],
+                [UserAddedEvent::class, $this->callback($isCallable)],
+                [UserRemovedEvent::class, $this->callback($isCallable)],
+                [PostLoginEvent::class, $this->callback($isCallable)]
+            );
+
+        $lm = $this->createListenerManager([], [], true);
+        $lm->setup();
+    }
+
+    public function testAddingToAutoGroups() {
+        $user = $this->createMock(IUser::class);
+        
+        $event = $this->createMock(UserCreatedEvent::class);
+        $event->expects($this->once())
+            ->method('getUser')
+            ->willReturn($this->testUser);
+
+        $this->groupManager->expects($this->once())
+            ->method('getUserGroups')
+            ->with($this->testUser)
+            ->willReturn([]);
+
+        $autogroup = $this->createMock(IGroup::class);
+        $autogroup->expects($this->once())->method('getGID')->willReturn('autogroup');
+        $autogroup->expects($this->once())->method('inGroup')->with($this->testUser)->willReturn(false);
+        $autogroup->expects($this->once())->method('addUser')->with($this->$testUser);
+
+        $this->groupManager->expects($this->once())
+            ->method('search')
+            ->with('autogroup', null, null)
+            ->willReturn($autogroup);
+
+        $callback = $this->initAndReturnCallback(['autogroup']);
+        call_user_func($callback, $event);
+
     }
 }
