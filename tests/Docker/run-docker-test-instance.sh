58c46b9ae024bd1b0732f9dc3cf47db9473526b5
Josua Hunziker <der@digitalwerker.ch>
2020-05-04 15:36:57 +0200

    update dockerfile and add test script

--- tests/Docker/run-docker-test-instance.sh
+++ tests/Docker/run-docker-test-instance.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+set -e
 
-## Use --build-arg INCUBATOR_VER=$(date +%F) to make sure nextcloud is freshly checked out each day
+docker build --build-arg DATE=$(date +%F) -t auto-groups-test ./tests/Docker
+
+echo "Starting Development Server..."
+ID=`docker run -d --rm -p 8080:8080 -v $(pwd):/server/apps/auto_groups auto-groups-test`
+
+trap finish INT
+function finish() {
+    echo "Finishing up..."
+    docker kill $ID
+    echo "Finished."
+}
+echo "Development server started."
+docker logs -f $ID
+
+
